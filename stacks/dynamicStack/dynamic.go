b1a3e0f012d9838f042400f3f9e627db2002a214
adarsh010c <adarsh010c@gmail.com>
2024-11-19 17:16:02 +0530

    dynamic stack added

---
+++ stacks/dynamicStack/dynamic.go
@@ -0,0 +1,68 @@
+package dynamicstack
+
+import "errors"
+
+//Create a dynamic stack
+//What is a dynamic stack
+// stack which has a variable length
+//It is based on linked list
+
+type Node[K comparable] struct {
+	data K
+	next *Node[K]
+}
+
+type DynamicStack[K comparable] struct {
+	top *Node[K]
+}
+
+func CreateDynamicStack[K comparable]() DynamicStack[K] {
+	return DynamicStack[K]{}
+}
+
+func (d *DynamicStack[K]) Push(value K) {
+	//create node
+	ptr := Node[K]{
+		data: value,
+	}
+
+	if d.top == nil {
+		d.top = &ptr
+	} else {
+		ptr.next = d.top
+		d.top = &ptr
+	}
+}
+
+func (d *DynamicStack[K]) Pop() (value K, err error) {
+	if d.top == nil {
+		return value, errors.New("stack is empty")
+	} else {
+		value = d.top.data
+		ptr := d.top.next
+		d.top = ptr
+		ptr = nil
+		return value, nil
+	}
+}
+
+func (d *DynamicStack[K]) Peek() (value K, err error) {
+	if d.top == nil {
+		return value, errors.New("stack is empty")
+	} else {
+		return d.top.data, nil
+	}
+}
+
+func (d *DynamicStack[K]) Display() ([]K, error) {
+	list := make([]K, 0)
+	if d.top == nil {
+		return list, errors.New("stack is empty")
+	} else {
+		for d.top != nil {
+			list = append(list, d.top.data)
+			d.top = d.top.next
+		}
+	}
+	return list, nil
+}
